082e773627f26ff099227a3c7e846ff3e126f6be
hausin <36941883+hausin@users.noreply.github.com>
2018-03-14 16:28:04 +0800

    Delete PChomeRCrawler.r

--- week_2/task_2/PChomeRCrawler.r
+++
@@ -1,7 +0,0 @@
-
-library(httr)
-
-url<-"https://ecshweb.pchome.com.tw/search/v3.3/all/results?q=%E3%84%87%E5%9E%8B%E6%9E%B6&page=1&sort=rnk/dc"
-res= GET(url)
-res_json= content(res)
-raw= data.frame(do.call(rbind,res_json$prods))
